c32eed1684e86e4fc2b9b16476eef565d6b01916
jkirmayer30 <jkirmayer30@stuy.edu>
2021-09-30 12:23:43 -0400

    07 and 08

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,12 @@
+public class Greet {
+
+public static void greet(String input){
+System.out.println("Why hello there"+input+"How do you do?");
+}
+
+public static void main(String[] args){
+System.out.println("no hablo queso");
+greet("jacob");
+}
+
+}
