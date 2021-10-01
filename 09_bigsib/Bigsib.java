60e74043aeb064cf20a7f53e85f83221ef57bcc4
jkirmayer30 <jkirmayer30@stuy.edu>
2021-10-01 12:26:20 -0400

    09

---
+++ 09_bigsib/Bigsib.java
@@ -0,0 +1,19 @@
+/*
+Discoveries:none
+Unresolved questions: none
+*/
+
+public class Bigsib {
+
+public static void greet(String input){
+System.out.println("Why hello there"+input+"How do you do?");
+}
+
+public static void main(String[] args){
+System.out.println("no hablo queso");
+greet("jacob");
+greet("josiah");
+greet("ameer");
+}
+
+}
---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,8 @@
+
+public class Greet {
+    public static void main( String[] args ) {
+        Bigsib.greet("Flim");
+        Bigsib.greet("Flam");
+        Bigsib.greet("Kazaam");
+    }
+}
