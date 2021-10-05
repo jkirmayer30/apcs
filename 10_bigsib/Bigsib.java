88d1d6f9cc231d8c06a03654b7bef2c3a5f2275e
Jacob Kirmayer <jacobkirmayer@gmail.com>
2021-10-04 20:31:33 -0400

    10

---
+++ 10_bigsib/Bigsib.java
@@ -0,0 +1,19 @@
+/*
+Discoveries:none
+Unresolved questions: none
+*/
+
+public class Bigsib {
+
+public static String greet(String input){
+return ("Why hello there"+input+"How do you do?");
+}
+
+public static void main(String[] args){
+System.out.println("no hablo queso");
+System.out.println(greet("jacob"));
+System.out.println(greet("josiah"));
+System.out.println(greet("ameer"));
+}
+
+}
