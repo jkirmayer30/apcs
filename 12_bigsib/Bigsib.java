6ac9f2686caf61c305b722a715b6a31df79a6757
Jacob Kirmayer <jacobkirmayer@gmail.com>
2021-10-05 20:48:14 -0400

    12

---
+++ 12_bigsib/Bigsib.java
@@ -0,0 +1,18 @@
+/*
+Apcs Hw 10 Jacob Kirmayer Period 6
+Discoveries:none
+Unresolved questions: none
+*/
+
+public class Bigsib {
+    public String greeting;
+
+    public void setHelloMsg(String message){
+        greeting = message;
+    }
+
+    public String greet(String input){
+        return (greeting+" "+input);
+    }
+
+}
