6f96e7adae4be6106157d868b4ab9113d94dab7a
jkirmayer30 <jkirmayer30@stuy.edu>
2021-10-19 12:27:29 -0400

    bank rob

---
+++ 19_BankAccount/cw/Teller.java
@@ -0,0 +1,9 @@
+public class Teller{
+  public static void main(String[] args){
+    BankAccount apple = new BankAccount();
+    apple.setAcctNum(100000000); //should be able to have this acct Number
+    apple.setPin((short) 1000); //should be able to have this pin
+    apple.deposit((double) -1000000000); //shouldn't be able to rob the bank
+    System.out.println(apple.toString());
+  }
+}
