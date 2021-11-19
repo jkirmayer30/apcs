c543f2d49e7d544d0fe349b8218d42434e4026d9
jkirmayer30 <jkirmayer30@stuy.edu>
2021-11-19 12:26:31 -0500

    yorpg

---
+++ YoRPG/Protagonist.java
@@ -0,0 +1,48 @@
+public class Protagonist {
+  public String _name;
+  public int _health;
+  public int _baseDamage;
+  public int _damageMult;
+
+  public Protagonist() {
+    _health = 50;
+    _baseDamage = 10;
+    _damageMult = 1;
+  }
+
+  public Protagonist(String name) {
+    _name = name;
+    _health = 50;
+    _baseDamage = 10;
+    _damageMult = 1;
+  }
+
+  public String getName() {
+    return _name;
+  }
+
+  public boolean isAlive() {
+    return _health > 0;
+  }
+
+  public int attack(Monster m) {
+    return m.getAttacked(_damageMult*_baseDamage);
+  }
+
+  public int getAttacked(int damage) {
+    if (Math.random()>0.5){
+      _health -= damage*_damageMult;
+      return damage*_damageMult;
+    }
+    return 0;
+  }
+
+  public void specialize() {
+    _damageMult = 2;
+  }
+
+  public void normalize() {
+    _damageMult = 1;
+  }
+
+}
