ad91a5022c1707e1f84c3415a71a25853e3b61e3
Jacob Kirmayer <jsk11235@jacob.local>
2022-03-01 22:48:34 -0500

    finally done

--- 68_knight/KnightTour.java
+++ 68_knight/KnightTour.java
@@ -1,8 +1,9 @@
-// Clyde Sinclair
-// APCS pd0
-// HW68 -- recursively probing for a closed cycle
-// 2022-02-28m
-// time spent:  hrs
+//        Team JEA: Jacob Kirmayer, Ethan Lam, Ariella Katz, Jacob Jr., Carl, Tom
+//        APCS
+//        HW68: ...and T-, Tr-, Tri-, Tries Again Until It's Done
+//        2022-03-01
+//        Time spent: 1.3 hrs
+
 
 /***
  * SKELETON
@@ -14,21 +15,33 @@
  * $ java KnightTour
  * $ java KnightTour [N]
  *
- * ALGO
  *
  * DISCO
- *
+ * System.exit stops the program
  * QCC
- *
+ * Why was this written with a system.exit function
  * Mean execution times for boards of size n*n:
- * n=5   __s    across __ executions
- * n=6   __s    across __ executions
- * n=7   __s    across __ executions
- * n=8   __s    across __ executions
+ * n=5   0.007s    across 10 executions
+ * n=6   0.08s    across 10 executions
+ * n=7   0.1s    across 10 executions
+ * n=8   very many s    across 1 executions
  *
  * POSIX PROTIP: to measure execution time from BASH, use time program:
  * $ time java KnightTour 5
  *
+ *ALGO
+ *
+ * Precond: The board is filled with “zeroes” (no knights have been placed yet)
+ * Postcond: The board is either filled with “ones” (a knight has been placed everywhere) or all possible states have been assessed and rejected.
+ * On an nxn grid:
+ * Choose a starting position
+ * Move the knight until
+ * No viable moves are left and spaces have still not been traveled to
+ * Return to the previous position
+ * Repeat step 2
+ * If the knight returns to its starting position, choose a new starting position
+ * All the spaces have been traveled to
+ * The knight can make a tour of the board.
  ***/
 
 
@@ -40,7 +53,7 @@ public class KnightTour
 {
   public static void main( String[] args )
   {
-    int n = 8;
+    int n = 5;
 
     try {
       n = Integer.parseInt( args[0] );
@@ -49,24 +62,27 @@ public class KnightTour
                           + n + "..." );
     }
 
-    TourFinder tf = new TourFinder( n );
 
-    //clear screen using ANSI control code
-    System.out.println( "[2J" );
-
-    //display board
-    System.out.println( tf );
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //for fixed starting location, use line below:
-    tf.findTour( 2, 2, 1 );
+
+      TourFinder tf = new TourFinder( n,System.currentTimeMillis() );
+
+      //clear screen using ANSI control code
+//      System.out.println( "[2J" );
+
+      //display board
+//      System.out.println( tf );
+      tf.findTour( 2, 2, 0 );
+      System.out.println( tf );
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //for random starting location, use lines below:
-    //int startX = //YOUR MATH CONSTRUCT FOR GENERATING A RANDOM LEGAL X VALUE
-    //int startY = //YOUR MATH CONSTRUCT FOR GENERATING A RANDOM LEGAL X VALUE
-    //tf.findTour( startX, startY, 1 );   // 1 or 0 ?
+//    int startX = (int) (Math.random()*n)+2;
+//    int startY = (int) (Math.random()*n)+2;
+//    tf.findTour( startX, startY, 0 );   // 1 or 0 ?
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -85,12 +101,13 @@ class TourFinder
   private int[][] _board;
   private int _sideLength; //board has dimensions n x n
   private boolean _solved = false;
+  private double start;
 
   //constructor -- build board of size n x n
-  public TourFinder( int n )
+  public TourFinder( int n,double _start )
   {
     _sideLength = n;
-
+    start = _start;
     //init 2D array to represent square board with moat
     _board = new int[n+4][n+4];
 
@@ -118,7 +135,6 @@ class TourFinder
     String retStr = "[0;0H";
     //emacs shortcut: C-q, then press ESC
     //emacs shortcut: M-x quoted-insert, then press ESC
-
     int i, j;
     for( i=0; i < _sideLength+4; i++ ) {
       for( j=0; j < _sideLength+4; j++ )
@@ -139,6 +155,7 @@ class TourFinder
     try {
       Thread.sleep(n);
     } catch( InterruptedException e ) {
+
       System.exit(0);
     }
   }
@@ -153,31 +170,46 @@ class TourFinder
    **/
   public void findTour( int x, int y, int moves )
   {
-    //delay(50); //slow it down enough to be followable
+//    delay(50);
 
     //if a tour has been completed, stop animation
-    if ( moves==n**2 ) System.exit(0);
-
-    //primary base case: tour completed
-    if ( ??? ) {
-      ???
-      System.out.println( this ); //refresh screen
+    if ( moves== Math.pow(_board.length-4,2) ) {
+//      System.out.println( this ); //refresh screen
+//      System.out.println("reached end");
+      System.out.println(System.currentTimeMillis()-start);
+      System.exit(0);
       return;
     }
+
+    //primary base case: tour completed
     //other base case: stepped off board or onto visited cell
-    if ( ??? ) {
+    if ( _board[x][y]!=0 ) {
       return;
     }
     //otherwise, mark current location
-    //and recursively generate tour possibilities from current pos
-    else {
 
+    //and recursively generate tour possibilities from current pos
+
+    else {
       //mark current cell with current move number
       _board[x][y] = moves+1;
-
-      System.out.println( this ); //refresh screen
-
-      //delay(1000); //uncomment to slow down enough to view
+//      System.out.println( this );
+      findTour(x+1,y+2,moves+1);
+      findTour(x+1,y-2,moves+1);
+      findTour(x-1,y+2,moves+1);
+      findTour(x-1,y-2,moves+1);
+      findTour(x+2,y+1,moves+1);
+      findTour(x+2,y-1,moves+1);
+      findTour(x-2,y+1,moves+1);
+      findTour(x-2,y-1,moves+1);
+      //If made it this far, path did not lead to tour, so back up...
+      // (Overwrite number at this cell with a 0.)
+      _board[x][y] = 0;
+//      System.out.println( this );
+      if (moves==0){
+        System.out.println(System.currentTimeMillis()-start);
+        System.exit(0);
+      }
 
       /******************************************
        * Recursively try to "solve" (find a tour) from
@@ -188,13 +220,8 @@ class TourFinder
        *     g . . . b
        *     . h . a .
       ******************************************/
-      ???
 
-      //If made it this far, path did not lead to tour, so back up...
-      // (Overwrite number at this cell with a 0.)
-        ???
 
-      System.out.println( this ); //refresh screen
     }
   }//end findTour()
 
