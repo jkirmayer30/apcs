301b2278e605b05f4d2a85ed3b5f996f0ee40ba2
Jacob Kirmayer <jsk11235@jacob.local>
2022-03-30 20:24:54 -0400

    tests and notes

--- 85_stack/Stckr.java
+++ 85_stack/Stckr.java
@@ -8,7 +8,8 @@ public class Stckr
 {
   public static boolean allMatched( String s )
   {
-    LLStack<String> unClosed = new LLStack<String>();
+//    LLStack<String> unClosed = new LLStack<String>();
+    ALStack<String> unClosed = new ALStack<String>();
     for (int i = 0; i<s.length(); i++){
       String character = s.substring(i, i+1);
       String popped = unClosed.pop();
