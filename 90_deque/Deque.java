2853e99071a7f679402c3dbf6597a6b24538c93f
jkirmayer30 <jkirmayer30@stuy.edu>
2022-04-12 12:34:26 -0400

    90

---
+++ 90_deque/Deque.java
@@ -0,0 +1,8 @@
+public interface Deque<T>{
+  public void addFirst(T x);
+  public void addLast(T x);
+  public T removeFirst(T x);
+  public T removeLast(T x);
+  public T getFirst(T x);
+  public T getLast(T x);
+}
